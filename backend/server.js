231852122bddb65ff60cae5a0a56338435ddfc50
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-01-25 03:57:40 +0600

    Auth Controllers Added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -7,11 +8,16 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // for parsing req.body
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   connectMongoDB();
 });
