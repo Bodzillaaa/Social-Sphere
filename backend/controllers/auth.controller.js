a8abe947557194c7ef95f907a14995f6de19d150
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-01-24 19:27:32 +0600

    Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    message: "Signup route works!",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    message: "Login route works!",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    message: "Logout route works!",
+  });
+};
